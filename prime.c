960f0b7efad0bc15806df0318c6da6cb24ea8037
rashmikumari1999 <67359719+rashmikumari1999@users.noreply.github.com>
2020-07-27 12:28:52 -0700

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,sum=0;
+    i=100
+    while(i<=100)
+    {
+        printf("%d",i);
+        sum=sum+int
+        i++
+        
+    }
+    printf("%d"s);
+
+}
+
+
